a0acf66ca1fd5e2c0e83650e649b01cac9bbd895
joseph <josephcotterell@hotmail.co.uk>
2023-07-25 16:37:38 +0100

    added nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="toggleButton">
-        <button></button>
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        ></button>
       </div>
       <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/experience">Experience</Link>
+        <Link to="/"> Home </Link>
+        <Link to="/projects"> Projects </Link>
+        <Link to="/experience"> Experience </Link>
       </div>
     </div>
   );
