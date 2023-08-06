768eb99cfe1f0c11e1bb666c75d96ed7f82a60d8
joseph <josephcotterell@hotmail.co.uk>
2023-08-06 19:06:07 +0100

    added experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,48 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2022 - 2023"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            University Of Birmingham Coding Bootcamp
+          </h3>
+          <p>Successfully passed</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2021 - 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            South Birmingham College
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            GCSE's Maths and English
+          </h4>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2022 - 2023"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+        >
+          <h3 className="vertical-timeline-element-title">Music Teacher</h3>
+          <h4 className="vertical-timeline-element-subtitle"></h4>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import "../styles/Home.css";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Joseph Cotterell</h2>
+        <div className="prompt">
+          <p>Software Engineer</p>
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, HTML, CSS, React Native, NPM, BootStrap, TailwindCSS,
+              StyledComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              NodeJS,JavaScript ExpressJS, GraphQL, ApolloServer, MySQL, MongoDB
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
