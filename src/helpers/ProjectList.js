c14b9505aea80314d1bd015d87baf3c9868ee96d
joseph <josephcotterell@hotmail.co.uk>
2023-08-08 12:17:48 +0100

    added projects page

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,28 @@
+import Proj1 from "../assets/Weather app photo (1).png";
+import Proj2 from "../assets/Screenshot (78).png";
+import Proj3 from "../assets/Screenshot (61).png";
+import Proj4 from "../assets/Screenshot (60).png";
+import Proj5 from "../assets/Screenshot (59).png";
+
+export const ProjectList = [
+  {
+    name: "Weather App",
+    image: Proj1,
+  },
+  {
+    name: "Ecommerce App",
+    image: Proj2,
+  },
+  {
+    name: "Note Taker App",
+    image: Proj3,
+  },
+  {
+    name: "Text Editor",
+    image: Proj4,
+  },
+  {
+    name: "Team Generator",
+    image: Proj5,
+  },
+];
