a235e8a7eb2feed46def737c6ad4a5a2d56974be
joseph <josephcotterell@hotmail.co.uk>
2023-08-24 19:03:16 +0100

    added project display

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,13 +1,14 @@
-import Proj1 from "../assets/Weather app photo (1).png";
-import Proj2 from "../assets/Screenshot (78).png";
-import Proj3 from "../assets/Screenshot (61).png";
-import Proj4 from "../assets/Screenshot (60).png";
-import Proj5 from "../assets/Screenshot (59).png";
+import Proj1 from "../assets/Proj1.png";
+import Proj2 from "../assets/Proj2.png";
+import Proj3 from "../assets/Proj3.png";
+import Proj4 from "../assets/Proj4.png";
+import Proj5 from "../assets/Proj5.png";
 
 export const ProjectList = [
   {
     name: "Weather App",
     image: Proj1,
+    skills: "JavaScript,HTML,CSS",
   },
   {
     name: "Ecommerce App",
@@ -18,7 +19,7 @@ export const ProjectList = [
     image: Proj3,
   },
   {
-    name: "Text Editor",
+    name: "Code Quiz",
     image: Proj4,
   },
   {
